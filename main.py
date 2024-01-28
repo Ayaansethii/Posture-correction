83f5155521f7ea77bf36aef8b7481d341fae5dfc
Taheer Khan <102434700+taheer6@users.noreply.github.com>
2024-01-28 17:19:15 -0500

    first commit

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ mp_drawing = mp.solutions.drawing_utils
 mp_holistic = mp.solutions.holistic
 
 webcam_feed = cv2.VideoCapture(0)
+current_frame = 0
+
 # Initiate holistic model
 with mp_holistic.Holistic(min_detection_confidence = 0.8, min_tracking_confidence = 0.8) as holistic:
     
